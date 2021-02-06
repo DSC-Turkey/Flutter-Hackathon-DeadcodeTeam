b163196dc01a22edaed531bf15dc642bc071f35c
Oğuzhan Atalay <oguzhnatly@gmail.com>
2021-02-07 01:26:17 +0300

    endpoints api config file created.

---
+++ lib/api/network/endpoints.dart
@@ -0,0 +1,6 @@
+class APIEndpoints {
+  static const String _BASE_URL =
+      "https://deadcode-hackathon-default-rtdb.firebaseio.com";
+
+  static const String USERS_API = _BASE_URL + "/users";
+}
